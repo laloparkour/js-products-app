0ca847a53ff0552da967fc31eb741a7bfd80ca1c
laloparkour <laloparkour09@gmail.com>
2020-06-16 12:16:03 -0600

    App Finished

--- App.js
+++ App.js
@@ -10,25 +10,41 @@ class UI {
     addProduct(product) {
         const productList = document.getElementById('product-list');
         const element = document.createElement('div');
-        element.innerHTML = "
+        element.innerHTML = `
         <div class="card text-center mb-4">
-        <div class="card-body">
-            <strong>    Product name</strong>: ${product.name}
-            <str    ong>Model</strong>: ${product.model}
+            <div class="card-body">
+                <strong>    Product name</strong>: ${product.name}
+                <strong>Model</strong>: ${product.model}
                 <strong>Product Price</strong>: ${product.price}
+                <a href="#" class="btn btn-danger" name="delete">Delete</a>
             </div>
         </div>
-        ";
+        `;
         productList.appendChild(element);
-
     }
 
-    deleteProduct() {
-
+    resetForm() {
+        document.getElementById('product-form').reset();
     }
 
-    showMessage() {
+    deleteProduct(element) {
+        if(element.name === 'delete'){
+            element.parentElement.parentElement.remove();
+            this.showMessage('Product Deleted Sucefully', 'success');
+        }
+    }
 
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-2`;
+        div.appendChild(document.createTextNode(message))
+        /* Showing in DOM */
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
     }
 }
 
@@ -43,11 +59,20 @@ document.getElementById('product-form')
             const product = new Product(name, model, price);
             
             const ui = new UI();
+
+            if(name === '' || model === '' || price === ''){
+                return ui.showMessage('Complete Fields Please', 'danger');
+            }
+
             ui.addProduct(product);
+            ui.resetForm();
+            ui.showMessage('Product Added Sucefully', 'success');
 
             event.preventDefault();//Nos permite cancelar el evento de refrescar la pagina
-
-
 });
 
-39.:04
+document.getElementById('product-list')
+    .addEventListener('click', function(e) { 
+        const ui =  new UI();
+        ui.deleteProduct(e.target);
+});
